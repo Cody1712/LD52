332b7515270eb4403c9364652a8512620eb8eae5
René <reneborger97@gmail.com>
2023-01-08 13:39:30 +0100

    [+] Game manager

--- GameJam_LD52/Assets/00_Scripts/GameManager.cs
+++ GameJam_LD52/Assets/00_Scripts/GameManager.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class GameManager : Manager<GameManager>
 {
+	[Header("Positioning")]
+    [SerializeField] GameObject waterPlane;
+    [SerializeField] float highTideHight = 1f;
+    [SerializeField] float lowTideHight = -1f;
+
+    [Header("Timing")]
+    [SerializeField] float highTideDuration = 10f;
+    [SerializeField] float lowTideDuration = 20f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +24,20 @@ public class GameManager : Manager<GameManager>
     {
         
     }
+
+#if UNITY_EDITOR
+
+	private void OnDrawGizmos()
+	{
+        Vector3 highTidePosition = new Vector3(0, highTideHight, 0);
+        Vector3 lowTidePosition = new Vector3(0, lowTideHight, 0);
+        Vector3 gizmoSize = new Vector3(2f,0.1f,2f);
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawLine(highTidePosition, lowTidePosition);
+
+        Gizmos.DrawCube(highTidePosition, gizmoSize);
+        Gizmos.DrawCube(lowTidePosition, gizmoSize);
+	}
+#endif
 }
